c4d0de33987be6141b82b5d4414ea0c4952bfb69
Yontemoon <monteyoon1998@gmail.com>
2023-06-06 11:56:38 -0700

    Logic is now all complete. Tabs are now all linked and properly working.

--- src/home.js
+++ src/home.js
@@ -1,50 +1,37 @@
-const content = document.getElementById('content');
 
-const homeTab = (() => {
-    const tabCtn = document.createElement('div');
-    tabCtn.id = "tab-ctn";
+function homeTab() {
+    const content = document.getElementById('content');
+    const menu = document.getElementById('menu-ctn');
+    const home = document.getElementById('home-ctn');
+    const contact = document.getElementById('contact-ctn');
 
-        const tabHome = document.createElement('p')
-        tabHome.id = "tabHome";
-        tabHome.textContent = "Home";
-        tabCtn.appendChild(tabHome);
-
-        const tabMenu = document.createElement('p');
-        tabMenu.id = 'tabHome';
-        tabMenu.textContent = 'Menu';
-        tabCtn.appendChild(tabMenu);
-
-        const tabContact = document.createElement('p');
-        tabContact.id='tabContact';
-        tabContact.textContent = "Contact";
-        tabCtn.appendChild(tabContact);
-
-    content.appendChild(tabCtn)
-
-    const titleHeader = document.createElement('header');
-    titleHeader.textContent = 'Casa De Monte Yoon';
-    titleHeader.id = 'titleHeader';
-    content.appendChild(titleHeader); 
     
+    const mainContent = document.createElement('div');
+    mainContent.setAttribute('id', 'home-ctn');
+    mainContent.setAttribute('class', 'mainContent')
+    
+    
+    // mainContent.classList.add('current');    
+    // mainContent.setAttribute('data-tab-content', '');
+    //THE REVIEW SECTION
     const reviewCtn = document.createElement('div');
+    reviewCtn.id = "reviewCtn";
         const review = document.createElement('p');
         review.textContent = "Lorem ipsum dolor sit amet, per no dolor tantas, udus iuvaret antiopam mel id. Alii nulla ius an, sit cu mollis ocurreret. Mea eu convenire assueverit sadipscing, quodsi malorum ne eam, in meliore ancillae sapientem pri. Et has case eripuit, eum minimum sensibus cu. Duo case dictas impedit an, nusquam mediocrem et per, ea fabellas recusabo eum"
         reviewCtn.appendChild(review);
-
         const reviewer = document.createElement('p');
         reviewer.textContent = "Monte Yoon";
         reviewCtn.appendChild(reviewer);
+    mainContent.appendChild(reviewCtn);
 
-    content.appendChild(reviewCtn);
 
+    //THE HOURS SECTION
     const hoursCtn = document.createElement('div');
     hoursCtn.id = 'hoursCtn';
-
     const hoursTitle = document.createElement('h3');
     hoursTitle.id = 'hoursTitle';
     hoursTitle.textContent = 'Hours';
     hoursCtn.appendChild(hoursTitle);
-
         const hourMonday = document.createElement('p');
         hourMonday.classList.add = 'hours';
         hourMonday.textContent = 'Monday 7am - 10pm';
@@ -80,25 +67,30 @@ const homeTab = (() => {
         hourSunday.textContent = 'Sunday CLOSED';
         hoursCtn.appendChild(hourSunday);
 
-        
-    content.appendChild(hoursCtn);
+    mainContent.appendChild(hoursCtn);
+
     
+    //THE LOCATION SECTION
     const locationCtn = document.createElement('div');
     locationCtn.id = "locationCtn";
-
         const locationTitle = document.createElement('h3');
         locationTitle.id ='LocationTitle'
         locationTitle.textContent = "Location";
         locationCtn.appendChild(locationTitle);
-
         const address = document.createElement('p');
         address.textContent = '10532 Fremont Ave N Seattle, WA';
         address.id = 'address';
         locationCtn.appendChild(address);
+   mainContent.appendChild(locationCtn);
+ 
+   content.appendChild(mainContent);
 
-    content.appendChild(locationCtn);
+   if (content.children.length > 2) {
+    const secondChild = content.children[1];
+    secondChild.remove();
+    }
 
-})();    
+};   
 
 
-export default homeTab;
+export { homeTab };
--- src/index.js
+++ src/index.js
@@ -1,108 +1,72 @@
-// const content = document.getElementById('content');
 
-// const homeTab = (() => {
-//     const tabCtn = document.createElement('div');
-//     tabCtn.id = "tab-ctn";
-
-//         const tabHome = document.createElement('p')
-//         tabHome.id = "tabHome";
-//         tabHome.textContent = "Home";
-//         tabCtn.appendChild(tabHome);
-
-//         const tabMenu = document.createElement('p');
-//         tabMenu.id = 'tabHome';
-//         tabMenu.textContent = 'Menu';
-//         tabCtn.appendChild(tabMenu);
-
-//         const tabContact = document.createElement('p');
-//         tabContact.id='tabContact';
-//         tabContact.textContent = "Contact";
-//         tabCtn.appendChild(tabContact);
-
-//     content.appendChild(tabCtn)
-
-//     const titleHeader = document.createElement('header');
-//     titleHeader.textContent = 'Casa De Monte Yoon';
-//     titleHeader.id = 'titleHeader';
-//     content.appendChild(titleHeader); 
-    
-//     const reviewCtn = document.createElement('div');
-//         const review = document.createElement('p');
-//         review.textContent = "Lorem ipsum dolor sit amet, per no dolor tantas, udus iuvaret antiopam mel id. Alii nulla ius an, sit cu mollis ocurreret. Mea eu convenire assueverit sadipscing, quodsi malorum ne eam, in meliore ancillae sapientem pri. Et has case eripuit, eum minimum sensibus cu. Duo case dictas impedit an, nusquam mediocrem et per, ea fabellas recusabo eum"
-//         reviewCtn.appendChild(review);
-
-//         const reviewer = document.createElement('p');
-//         reviewer.textContent = "Monte Yoon";
-//         reviewCtn.appendChild(reviewer);
-
-//     content.appendChild(reviewCtn);
-
-//     const hoursCtn = document.createElement('div');
-//     hoursCtn.id = 'hoursCtn';
-
-//     const hoursTitle = document.createElement('h3');
-//     hoursTitle.id = 'hoursTitle';
-//     hoursTitle.textContent = 'Hours';
-//     hoursCtn.appendChild(hoursTitle);
-
-//         const hourMonday = document.createElement('p');
-//         hourMonday.classList.add = 'hours';
-//         hourMonday.textContent = 'Monday 7am - 10pm';
-//         hoursCtn.appendChild(hourMonday);
-
-//         const hourTuesday = document.createElement('p');
-//         hourTuesday.classList.add = 'hours';
-//         hourTuesday.textContent = 'Tuesday 7am - 10pm';
-//         hoursCtn.appendChild(hourTuesday);
-
-//         const hourWednesday = document.createElement('p');
-//         hourWednesday.classList.add = 'hours';
-//         hourWednesday.textContent = 'Wednesday 7am - 10pm';
-//         hoursCtn.appendChild(hourWednesday);
-
-//         const hourThursday = document.createElement('p');
-//         hourThursday.classList.add = 'hours';
-//         hourThursday.textContent = 'Thursday 7am - 10pm';
-//         hoursCtn.appendChild(hourThursday);
-
-//         const hourFriday = document.createElement('p');
-//         hourFriday.classList.add = 'hours';
-//         hourFriday.textContent = 'Friday 7am - 10pm';
-//         hoursCtn.appendChild(hourFriday);
-
-//         const hourSaturday = document.createElement('p');
-//         hourSaturday.classList.add = 'hours';
-//         hourSaturday.textContent = 'Saturday CLOSED';
-//         hoursCtn.appendChild(hourSaturday);
-
-//         const hourSunday = document.createElement('p');
-//         hourSunday.classList.add = 'hours';
-//         hourSunday.textContent = 'Sunday CLOSED';
-//         hoursCtn.appendChild(hourSunday);
-
-        
-//     content.appendChild(hoursCtn);
-    
-//     const locationCtn = document.createElement('div');
-//     locationCtn.id = "locationCtn";
-
-//         const locationTitle = document.createElement('h3');
-//         locationTitle.id ='LocationTitle'
-//         locationTitle.textContent = "Location";
-//         locationCtn.appendChild(locationTitle);
-
-//         const address = document.createElement('p');
-//         address.textContent = '10532 Fremont Ave N Seattle, WA';
-//         address.id = 'address';
-//         locationCtn.appendChild(address);
-
-//     content.appendChild(locationCtn);
-
-// })();    
+import { headerPage } from "./header.js";
+import { menuTab } from "./menu.js"
+import { contactPageLoad } from "./contact.js";
+import{ homeTab } from "./home.js";
 
 
-import Home from "./home.js";
+// const tabs = document.querySelectorAll('[data-tab-target]')
+// const tabContents = document.querySelectorAll('[data-tab-content]')
 
-Home();
+
+// init();
+
+const init =(() => {
+    headerPage();
+    homeTab();
+    addNavEvents();
+
+})();
+
+function addNavEvents() {
+    const homeBtn = document.getElementById('homeBtn');
+    const menuBtn = document.getElementById('menuBtn');
+    const contactBtn = document.getElementById('contactBtn');
+
+    if(homeBtn) {
+        homeBtn.addEventListener('click', homeTab, false);
+
+    }
+    if(menuBtn) {
+        menuBtn.addEventListener('click', menuTab, false);
+    }
+
+    if(contactBtn) {
+        contactBtn.addEventListener('click', contactPageLoad, false);
+
+    }
+}
 
 
+// tabs.forEach((tab) =>
+//     tab.addEventListener('click', () => {
+//         const target = document.querySelector(tab.dataset.tabTarget);
+//         tabContents.forEach((tabContent) => {
+//             tabContent.classList.remove("current");
+//         })
+//         target.classList.add('current');
+//     })
+// )
+
+
+
+
+// tabHome.addEventListener('click', () => {
+//     // if (menuTab === true) {
+//     //     content.appendChild(container);
+//     //     menuTab = false;
+
+//     // }
+//     homeTab;
+// })
+
+// const tabMenu = document.getElementById('tabMenu');
+// tabMenu.addEventListener('click', () => {
+//     menuTab;
+// })
+
+// const getContactPage = document.getElementById('tabContact');
+
+// getContactPage.addEventListener('click', () => {
+//     contactPageLoad;
+// })
